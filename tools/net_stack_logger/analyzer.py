bfb44fd7b01539acbdfa06d168ca70825fa2e8ae
goda <goda@bloodorange.cache3.t-lab.cs.teu.ac.jp>
2011-01-05 00:39:47 +0900

    new analyzer ....

--- tools/net_stack_logger/analyzer.py
+++ tools/net_stack_logger/analyzer.py
@@ -1,89 +1,126 @@
-#!/usr/bin/env python
-
+from optparse import OptionParser
+import numpy
+import csv
 import sys
 
-class Packet(object):
 
-    def __init__(self,log):
-        self.mem_addr = log["skb"]
-        self.cpu      = log["cpu"]
-        self.eth_prot = log["eth_protocol"]
-        self.ip_prot  = log["ip_protocol"]
-        self.ip_saddr = log["ip_saddr"]
-        self.ip_daddr = log["ip_daddr"]
-        self.tp_sport = log["tp_sport"]
-        self.tp_dport = log["tp_dport"]
-        
-        self.times    = {log["func"] : int(log["time"])}
-        self.delta_time = {}
+IPPROTO_UDP = '17'
+IPPROTO_TCP = '6'
 
-        
-    def pushLog(self,log):
-        self.times[log["func"]] = int(log["time"])
 
-        
+NSL_NETIF_RECEIVE_SKB =  '1'
+NSL_ENQUEUE_TO_BACKLOG = '2'
+NSL_SKB_DEQUEUE = '6'
+NSL_SKB_COPY = '7'
+NSL_BEGIN_INET_RECVMSG = '13'
+NSL_END_INET_RECVMSG  = '14'
+
+
+opt = OptionParser()
+opt.add_option('-t','--tcp',action='store_true')
+opt.add_option('-u','--udp',action='store_true')
+opt.add_option('-s','--sock',action='store_true')
+
+
+opts , argv = opt.parse_args()
+
+
+protocol = ''
+head_func = ''
+tail_func = ''
+func = []
+tmp = []
+
+
+if opts.tcp:
+    protocol = IPPROTO_TCP
+    head_func = NSL_NETIF_RECEIVE_SKB
+    tail_func = NSL_SKB_COPY
+    fucn = []
+    tmp = []
+    exit(0)
     
-    def calucTime(self):
-        KEY,VAL = 0,1
-        
-        t = sorted(self.times.items(), key=lambda x : x[1])
+elif opts.udp:
+    protocol = IPPROTO_UDP
+    head_func = NSL_NETIF_RECEIVE_SKB
+    tail_func = NSL_SKB_COPY
+    func = ["", 'netif_receive_skb', 'enqueue_to_backlog', '__netif_receive_skb',
+            'ip_rcv', 'sk_data_ready', 'skb_dequeue', 'skb_copy', 'to_syscall']
+    tmp = [[] for x in range(8)]
 
-        self.delta_time[t[0][KEY]] = 0
-        prev = t[0]
+elif opts.sock:
+    protocol = '0'
+    head_func = NSL_BEGIN_INET_RECVMSG
+    tail_func = NSL_END_INET_RECVMSG
+    func = ["","begin_inet_recvmsg","end_inet_recvmsg","cnt"]
+    tmp = [[] for x in range(3)]
 
-        for curr in t[1:]:
-            print "%d - %d = %d"%(curr[VAL] , prev[VAL] , (curr[VAL] - prev[VAL]))
-            self.delta_time[curr[KEY]] = curr[VAL] - prev[VAL]
-            prev = curr
 
     
-    def __str__(self):
-        str = ",".join([self.mem_addr, self.ip_saddr, self.ip_daddr])
+PROTOS = {0:'sock',17:'udp',6:'tcp'}
 
-        KEY,VAL = 0,1
-        t = sorted(self.times.items(), key=lambda x : x[1])
-        times = "{ " + ', '.join( map(lambda x : "%s : %d"%(x[KEY], self.times[x[KEY]]),t) ) + " }"
-        delta_time = "{ " + ', '.join( map(lambda x : "%s : %d"%(x[KEY], self.delta_time[x[KEY]]),t) ) + " }"
-        str = str + "\n" + times.__str__() + "\n" + delta_time.__str__()
-        return str
+flow = {}
+
+reader = csv.DictReader(sys.stdin)
 
 
-
-def parseParameter(log):
-    return dict( map(lambda x : x.split(":"), log.split()) )
-
-if __name__ == "__main__":
-    if len(sys.argv) <= 2:
-        print "please file name"
-        
-
-    packets = {}
-
-    print "loading file..."
-    f = open(sys.argv[1])
-    log = f.readlines()
-    f.close()
-    print "load complete."
-
-    print "parsing parameter..."
-    log = map(parseParameter, log[1:])
-    print "parsing complete."
-
-    print "..."
-    for l in log:
-        if l["skb"] in packets:
-            packets[l["skb"]].pushLog(l)
-        else:
-            packets[l["skb"]] = Packet(l)
-    print "complete"
-
-    print "..."
-    for v in packets.values():
-        v.calucTime()
-    print "complete"
-        
-    for v in packets.values():
-        print v
+for packet in reader:
     
+    if packet['ip_protocol'] != protocol:
+        continue
+
+    id = packet['id']
     
-    print "total packets : %d"%len(packets)
+    if flow.has_key(id):
+        flow[id].append(packet)
+    else:
+        flow[id] = [packet]
+        
+    if packet['func'] == tail_func:
+        if flow[id][0]['func'] == head_func:
+            first = flow[id][0]
+            
+            print "[%s/%s]," % (PROTOS[int(packet['ip_protocol'])], packet['tp_sport' if packet['ip_protocol'] == '0' else 'tp_dport']),
+
+            
+            for i,p in enumerate(flow[id]):
+                print "%d," % (int(p['time']) - int(first['time'])),
+                tmp[i].append((int(p['time']) - int(first['time'])))
+                first = p
+                i += 1
+
+                
+            if opts.tcp or opts.udp:
+                first = flow[id][0]
+                dequeue = None
+                
+                for p in flow[id]:
+                    if p['func'] == NSL_SKB_DEQUEUE:
+                        dequeue = p
+                        break
+                    
+                print "%d," % (int(dequeue['time']) - int(flow[id][0]['time'])),
+                tmp[-1].append((int(dequeue['time']) - int(flow[id][0]['time'])))
+
+            elif opts.sock:
+                first = flow[id][0]
+                for p in flow[id][1:]:
+                    print "%d," % (int(p['cnt']) - int(first['cnt'])),
+                    tmp[-1].append(int(p['cnt']) - int(first['cnt']))
+                    
+            print
+                
+        del flow[id]
+
+
+if tmp[-2] == []:
+    func.remove('enqueue_to_backlog')
+    tmp.pop(-2)
+    
+print
+print ','.join(func)
+print "[average],", ', '.join(  map(str, map(numpy.average, tmp) ) )
+print "[median],",  ', '.join(  map(str, map(numpy.median,  tmp) ) )
+print "[max],",     ', '.join(  map(str, map(numpy.max,     tmp) ) )
+print "[min],",     ', '.join(  map(str, map(numpy.min,     tmp) ) )
+print "[varance],", ', '.join(  map(str, map(numpy.var,     tmp) ) )
