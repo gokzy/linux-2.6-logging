d4600d123d1acae68a7b64695cfe34c0ff64508f
goda <goda@natsumikan.cache3.t-lab.cs.teu.ac.jp>
2011-01-27 20:13:05 +0900

    New version analyzer.py. Currentry, it's only TCP.

---
+++ tools/net_stack_logger/new_analyzer.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+import csv
+import sys
+
+NSL_START_LOGGING         = 1
+NSL_POLLING               = 2
+NSL_GRO_PROCESS           = 3
+NSL_RPS_ENQUEUE_BACKLOG   = 4
+NSL_RPS_DEQUEUE_BACKLOG   = 5
+
+NSL_IP_RCV                = 6
+NSL_ENQUEUE_FRAGMENTS     = 7
+
+NSL_TCP_V4_RCV            = 8
+NSL_TCP_ENQUEUE_BACKLOG   = 9
+NSL_TCP_V4_DO_RCV         = 10
+NSL_TCP_DEQUEUE_BACKLOG   = 11
+NSL_FASTPATH_DATA_READY   = 12
+NSL_SLOWPATH_DATA_READY   = 13
+NSL_OFOPATH_DATA_READY    = 14
+NSL_SYSCALL_PROCESS_START = 15
+NSL_COPY_SKB_COMPLETE     = 16
+NSL_DEQUEUE_RECEIVE_QUEUE = 17
+
+NSL_UDP_RCV               = 18
+NSL_UDP_DATA_READY        = 19
+
+NSL_TCP_FOUND_FIN_OK        = 30
+NSL_TCP_BEFORE_CLEANUP_RBUF = 31
+NSL_TCP_AFTER_CLEANUP_RBUF  = 32
+NSL_TCP_OUT                 = 33
+NSL_TCP_RECV_URG            = 34
+
+
+class NoDefineError(Exception):
+    pass
+
+class ReceiveFlow(dict):
+
+    def __init__(self):
+        self.path = []
+        self.exit = False
+        self.porcess_span = []
+
+        
+    def __setitem__(self, key, val):
+        super(ReceiveFlow,self).__setitem__(key,val)
+
+        if key == NSL_DEQUEUE_RECEIVE_QUEUE:
+            self.path = self.path_generator()
+        
+            if self.path == None:
+                return False
+
+
+            if self.is_complete_data():
+                self.calc_process_span()
+                self.exit = True
+            else:
+                pass
+
+            
+    def calc_process_span(self):
+        prev = self.__getitem__(self.path[0])
+
+        for curr_key in self.path[1:]:
+            curr = self.__getitem__(curr_key)
+            
+            self.process_span += int(curr['time']) - int(prev['time'])
+            prev = curr
+
+
+class TcpReceiveFlow(ReceiveFlow):
+
+    def __init__(self):
+        super(TcpReceiveFlow, self).__init__()
+
+    
+    def is_complete_data(self):
+        for func in self.path:
+            if not func in self.keys():
+                return False
+
+        return True
+
+    
+    def path_generator(self):
+        path = []
+        flow = 0
+        
+        if NSL_RPS_ENQUEUE_BACKLOG in self.keys():
+            path += [1,2,3,4,5,6]
+        else:
+            path += [1,2,3,5,6]
+
+        if NSL_TCP_ENQUEUE_BACKLOG in self.keys():
+            path += [8,9,11,10]
+        else:
+            path += [8,10]
+
+        if NSL_FASTPATH_DATA_READY in self.keys():
+            path += [12]
+            
+        elif NSL_SLOWPATH_DATA_READY in self.keys():
+            path += [13]
+
+        elif NSL_OFOPATH_DATA_READY in self.keys():
+            path += [14]
+        else:
+            return None
+
+        path += [15,16,17]
+
+        return path, flow
+
+
+
+flows = {}
+process_times = {}
+        
+reader = csv.DictReader(sys.stdin)
+
+for pkt in reader:
+    
+    if (pkt['ip_protocol'] != '6' or pkt['tp_dport'] != '5001') and pkt['func'] != '1':
+
+        continue
+
+    id = pkt["skb_id"]
+    
+    if id in flows:
+        flows[id][int(pkt['func'])] = pkt
+    else:
+        flows[id] = TcpReceiveFlow()
+        flows[id][int(pkt['func'])] = pkt
+
+        
+    if flows[id].exit:
+        process_times[tuple(flows[id].path)] = flows[id].process_span
+        del flows[id]
+
+for key,val in flows.items():
+    print key
+    print val
+
